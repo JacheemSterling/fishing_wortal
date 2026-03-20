419cb0a1201b191bd6b934bb354cf329ac5e67a8
Błażej Jachimek <blazkojo@gmail.com>
2026-03-20 16:29:38 +0100

    dooddania

--- users/apps.py
+++ users/apps.py
@@ -5,4 +5,4 @@ class UsersConfig(AppConfig):
     name = 'users'
 
     def ready(self):
-        import users.signals  # To jest kluczowe!
+        import users.signals  
--- users/views.py
+++ users/views.py
@@ -12,7 +12,7 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save() # To stworzy użytkownika (a sygnały stworzą profil!)
+            form.save() 
             username = form.cleaned_data.get('username')
             messages.success(request, f'Konto stworzone dla {username}! Możesz się zalogować.')
             return redirect('login')
